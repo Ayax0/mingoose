ed029e060dfc1e5dc97350d71bfc90895130145d
Simon Gander <simon.gander@ioz.ch>
2024-07-31 11:58:36 +0200

    fix: update type import

--- src/utils/model.ts
+++ src/utils/model.ts
@@ -1,6 +1,6 @@
 import { ObjectId } from "mongodb";
 import type { WithId } from "mongodb";
-import type { ObjectIdLike } from "../types/mongodb";
+import type { ObjectIdLike } from "../types";
 
 export function parseObjectIdLike<DocType>(id: ObjectIdLike) {
   return { _id: new ObjectId(id) as WithId<DocType>["_id"] };
